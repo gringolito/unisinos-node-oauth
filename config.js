261c62b3b6cc86cd4262924b80b784cae1f37cec
Filipe Utzig <filipe.utzig@datacom.ind.br>
2019-03-25 20:03:37 -0300

    Handle authentication request on Google API
    Signed-off-by: Filipe Utzig <filipe.utzig@datacom.ind.br>

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'googleAuth' : {
+        'clientID'     : '499745103799-4bheeqtg6s96lvjf92jvp93a3tee8pco.apps.googleusercontent.com',
+        'clientSecret' : '2Z5zoiudHHYj7Yb-7VxVcCB8',
+        'callbackURL'  : 'http://localhost:4000/auth/google/callback'
+    }
+};
